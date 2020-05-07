bd13c70f846996f08cab555e2c48a7b4b83846d3
bob4t1gd3 <bob4t1gd3@yandex.ru>
2020-05-07 21:01:20 +0300

    first commit
    add simple structure to ny todo app
    + some html\scss

---
+++ todo-app/src/components/List/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./List.scss";
+
+const List = ({ items }) => {
+    return (
+        <ul className="list">
+            {
+                items.map(item => (
+                    <li className="active">
+                        <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i> }</i>
+                        <span>{item.name}</span>
+                    </li>
+                ))
+            }
+        </ul>
+    );
+}
+
+export default List;
