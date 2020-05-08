bcddf976ee5fe261b0cc9094d3ea86928b46b05f
bob4t1gd3 <bob4t1gd3@yandex.ru>
2020-05-08 06:04:17 +0300

    add popup for addList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import List from './components/List'
+import AddList from './components/AddList'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className='todo__sidebar'>
         <List items={[
           {
-            icon: (<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="list" class="svg-inline--fa fa-list fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M80 368H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm0-320H16A16 16 0 0 0 0 64v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16V64a16 16 0 0 0-16-16zm0 160H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm416 176H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path></svg>
+            icon: (<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="list" className="svg-inline--fa fa-list fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M80 368H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm0-320H16A16 16 0 0 0 0 64v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16V64a16 16 0 0 0-16-16zm0 160H16a16 16 0 0 0-16 16v64a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-64a16 16 0 0 0-16-16zm416 176H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path></svg>
             ),
             name: "Все задачи"
           },
@@ -26,10 +27,13 @@ function App() {
             },
             {
               color: 'yellow',
-              name: "Покупки"
+              name: "Покупки",
+              active: true
             }
           ]}
+          isRemovable
         />
+        <AddList />
       </div>
       <div className="todo__tasks"></div>
     </div>  
--- todo-app/src/components/List/index.jsx
+++ todo-app/src/components/List/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import "./List.scss";
 
-const List = ({ items }) => {
+const List = ({ items, isRemovable, onClick }) => {
     return (
-        <ul className="list">
+        <ul onClick={onClick} className="list">
             {
-                items.map(item => (
-                    <li className="active">
+                items.map((item, index) => (
+                    <li key={index} className={classNames(item.className, {'active': item.active})}>
                         <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i> }</i>
                         <span>{item.name}</span>
                     </li>
